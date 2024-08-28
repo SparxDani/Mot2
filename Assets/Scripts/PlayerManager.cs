cbd9e428a3ad7f55ca7abc39ac401981477fc915
SparxDani <86084707+SparxDani@users.noreply.github.com>
2024-08-28 13:00:05 -0500

    Anims and 2nd jump

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -12,28 +12,89 @@ public class PlayerManager : MonoBehaviour
     public SpriteRenderer sprite;
     public LayerMask groundLayerMask;
     private bool IsGrounded;
+    private bool _Jump;
+    private bool _canJump;
+    private bool _hasDoubleJump;
+    private float horizontalInput;
+
+    public Animator animator;
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
+
     private void Update()
     {
-        IsGrounded = Physics2D.Raycast(GroundCheck.position, Vector2.down, groundCheckDistance, groundLayerMask);
-        if (IsGrounded && Input.GetKeyDown(KeyCode.Space))
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (horizontalInput != 0)
         {
-            body.velocity = new Vector2(body.velocity.x, playerJumpForce);
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isIdle", false);
+        }
+        else
+        {
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isIdle", true);
+        }
+
+        if (horizontalInput > 0)
+        {
+            sprite.flipX = false;
+        }
+        else if (horizontalInput < 0)
+        {
+            sprite.flipX = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _Jump = true;
         }
     }
 
     private void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput * playerSpeed, body.velocity.y);
+        CheckRaycast();
 
-        
-
+        if (_Jump)
+        {
+            if (_canJump)
+            {
+                body.AddForce(Vector2.up * playerJumpForce, ForceMode2D.Impulse);
+                _canJump = false;
+                _Jump = false;
+                animator.SetTrigger("Jump");
+            }
+            else if (_hasDoubleJump)
+            {
+                body.AddForce(Vector2.up * playerJumpForce, ForceMode2D.Impulse);
+                _hasDoubleJump = false;
+                _Jump = false;
+                //animator.SetTrigger("DoubleJump");
+            }
+        }
     }
 
+    private void CheckRaycast()
+    {
+        Debug.DrawRay(GroundCheck.position, Vector2.down * groundCheckDistance, Color.cyan);
+        RaycastHit2D hit = Physics2D.Raycast(GroundCheck.position, Vector2.down, groundCheckDistance, groundLayerMask);
+
+        if (hit.collider != null)
+        {
+            _canJump = true;
+            _hasDoubleJump = true;
+            animator.SetBool("isJumping", true); 
+        }
+        else
+        {
+            _canJump = false;
+            animator.SetBool("isJumping", false);
+        }
+    }
 }
